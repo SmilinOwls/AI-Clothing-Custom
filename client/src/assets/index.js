7916a7c535ae6c0e174d92a7725ab34606f83837
20120234 <20120234@student.hcmus.edu.vn>
2023-07-23 00:30:34 +0700

    preparation for code

---
+++ client/src/assets/index.js
@@ -0,0 +1,15 @@
+import ai from './ai.png';
+import download from './download.png';
+import file from './file.png';
+import logoShirt from './logo-tshirt.png';
+import stylistShirt from './stylist-tshirt.png';
+import swatch from './swatch.png';
+
+export {
+    ai,
+    download,
+    file,
+    logoShirt,
+    stylistShirt,
+    swatch,
+}
